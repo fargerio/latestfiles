5e4d48d1040b250b1c9c653da131243adce0c3c9
Florian Zeidler <fz@fargerio.de>
2014-11-15 15:33:53 -0500

    changed order, write to temp file first in case of longer search operation

--- latestfiles.sh
+++ latestfiles.sh
@@ -1,39 +1,44 @@
 # !/bin/sh 
 # This script will generate a html document listing the newest files in the given filesystem
 # Arguments: $1 Directory for search
-#			 $2 output file
+#			 $2 output file (will be overwritten if exists)
 # 			 $3 number of days to show
 DIRECTORY=$1
 OUTPUT_FILE=$2
 DAYS=$3
+TEMP_FILE="temp.html"
 
 START="start.$$"
 END="end.$$"
 
-echo "<html><body>" > $OUTPUT_FILE
+echo "<html><body>" > $TEMP_FILE
 
-while [ $DAYS -ge 1 ]
+SDATE=$(date +"%Y-%m-%d" --date="1 days ago")
+touch --date "$SDATE" $START
+echo "<h1>Today</h1><ul>" >> $TEMP_FILE
+find $DIRECTORY -newer $START -type f \( ! -regex '.*/\..*' \) -printf "<li class=\"%y\"><a href=\"%p\">%f</a> - %kk - %Tk:%TM</li></br>" >> $TEMP_FILE
+echo "</ul>" >> $TEMP_FILE
+I=1
+while [ $I -le $DAYS ]
 do
-	SDATE=$(date +"%Y-%m-%d" --date="$DAYS days ago")
-	DAYS=$(( DAYS-1 ))
-	EDATE=$(date +"%Y-%m-%d" --date="$DAYS days ago")
+	EDATE=$(date +"%Y-%m-%d" --date="$I days ago")
+	I=$(( I+1 ))
+	SDATE=$(date +"%Y-%m-%d" --date="$I days ago")
 
 	touch --date "$SDATE" $START
 	touch --date "$EDATE" $END
 
-	echo "<h1>$SDATE</h1><ul>" >> $OUTPUT_FILE
+	echo "<h1>$SDATE</h1><ul>" >> $TEMP_FILE
 
-	find $DIRECTORY -newer $START \! -newer $END -type f \( ! -regex '.*/\..*' \) -printf "<li class=\"%y\"><a href=\"%p\">%f</a> - %kk - %Tk:%TM</li></br>" >> $OUTPUT_FILE
+	find $DIRECTORY -newer $START \! -newer $END -type f \( ! -regex '.*/\..*' \) -printf "<li class=\"%y\"><a href=\"%p\">%f</a> - %kk - %Tk:%TM</li></br>" >> $TEMP_FILE
 
-	echo "</ul>" >> $OUTPUT_FILE
+	echo "</ul>" >> $TEMP_FILE
 done
 
-SDATE=$(date +"%Y-%m-%d" --date="1 days ago")
-touch --date "$SDATE" $START
-echo "</ul><h1>Today</h1><ul>" >> $OUTPUT_FILE
-find $DIRECTORY -newer $START -type f \( ! -regex '.*/\..*' \) -printf "<li class=\"%y\"><a href=\"%p\">%f</a> - %kk - %Tk:%TM</li></br>" >> $OUTPUT_FILE
 
-echo "</ul></body></html>" >> $OUTPUT_FILE
+echo "</ul></body></html>" >> $TEMP_FILE
 
 /bin/rm -f "$START" 
 /bin/rm -f "$END"
+rm $OUTPUT_FILE
+mv $TEMP_FILE $OUTPUT_FILE
