18b37ac6c06165acaee605cbf18fec536f25407e
Florian Zeidler <fz@fargerio.de>
2014-11-18 00:45:07 -0500

    named parameters, option to replace base path, optional verbosity

--- latestfiles.sh
+++ latestfiles.sh
@@ -1,23 +1,68 @@
 # !/bin/sh 
 # This script will generate a html document listing the newest files in the given filesystem
-# Arguments: $1 Directory for search
-#			 $2 output file (will be overwritten if exists)
-# 			 $3 number of days to show
-DIRECTORY=$1
-OUTPUT_FILE=$2
-DAYS=$3
+# Arguments:
+#	-p [searchpath]    : Path in which to search
+#	-o [output file]   : file to which to write the list
+#   -d [number of days]: number of days in the past to search
+#   -b [new_basepath]  : replace the searchpath in file links with new_basepath
+#   -v 				   : verbose output during execution
+#
+DAYS=7
+REPLACEBASELINK=""
+while getopts ":p:o:d:b:v" optname
+  do
+    case "$optname" in
+      "p")
+		DIRECTORY=$OPTARG
+      	;;
+      "o")
+		OUTPUT_FILE=$OPTARG
+        ;;
+      "d")
+		DAYS=$OPTARG
+        ;;
+      "b")
+		REPLACEBASELINK=$OPTARG
+		;;
+	  "v")
+		VERBOSE=true
+		;;
+      "?")
+        echo "Unknown option $OPTARG"
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        ;;
+      *)
+      # Should not occur
+        echo "Unknown error while processing options"
+        ;;
+    esac
+  done
+
+if $VERBOSE; then
+	echo "   output: $OUTPUT_FILE"
+	echo "     days: $DAYS"
+	echo "  replace: $REPLACEBASELINK"
+fi
+
 TEMP_FILE="temp.html"
 
 START="start.$$"
 END="end.$$"
 
-echo "<html><body>" > $TEMP_FILE
+echo "<html><body>" > "$TEMP_FILE"
 
 SDATE=$(date +"%Y-%m-%d" --date="1 days ago")
-touch --date "$SDATE" $START
-echo "<h1>Today</h1><ul>" >> $TEMP_FILE
-find $DIRECTORY -newer $START -type f \( ! -regex '.*/\..*' \) -printf "<li class=\"%y\"><a href=\"%p\">%f</a> - %kk - %Tk:%TM</li></br>" >> $TEMP_FILE
-echo "</ul>" >> $TEMP_FILE
+touch --date "$SDATE" "$START"
+echo "<h1>Today</h1><ul>" >> "$TEMP_FILE"
+
+if $VERBOSE; then
+	echo "searching: $DIRECTORY"
+fi
+
+find $DIRECTORY -newer $START -type f \( ! -regex '.*/\..*' \) -printf "<li class=\"%y\"><a href=\"%p\">%f</a> - %kk - %Tk:%TM</li></br>" >> "$TEMP_FILE"
+echo "</ul>" >> "$TEMP_FILE"
 I=1
 while [ $I -le $DAYS ]
 do
@@ -25,20 +70,33 @@ do
 	I=$(( I+1 ))
 	SDATE=$(date +"%Y-%m-%d" --date="$I days ago")
 
-	touch --date "$SDATE" $START
-	touch --date "$EDATE" $END
+	touch --date "$SDATE" "$START"
+	touch --date "$EDATE" "$END"
 
-	echo "<h1>$SDATE</h1><ul>" >> $TEMP_FILE
+	echo "<h1>$SDATE</h1><ul>" >> "$TEMP_FILE"
 
-	find $DIRECTORY -newer $START \! -newer $END -type f \( ! -regex '.*/\..*' \) -printf "<li class=\"%y\"><a href=\"%p\">%f</a> - %kk - %Tk:%TM</li></br>" >> $TEMP_FILE
+	if $VERBOSE; then
+		echo "searching: $SDATE - $(( DAYS-I+2 )) day(s) to go"
+	fi
 
-	echo "</ul>" >> $TEMP_FILE
+	find $DIRECTORY -newer $START \! -newer $END -type f \( ! -regex '.*/\..*' \) -printf "<li class=\"%y\"><a href=\"%p\">%f</a> - %kk - %Tk:%TM</li></br>" >> "$TEMP_FILE"
+
+	echo "</ul>" >> "$TEMP_FILE"
 done
 
 
-echo "</ul></body></html>" >> $TEMP_FILE
+echo "</ul></body></html>" >> "$TEMP_FILE"
 
 /bin/rm -f "$START" 
 /bin/rm -f "$END"
-rm $OUTPUT_FILE
-mv $TEMP_FILE $OUTPUT_FILE
+
+if [ $REPLACEBASELINK == "" ]; then
+	mv -f "$TEMP_FILE" "$OUTPUT_FILE"
+	echo "not replacing"
+else
+	if $VERBOSE; then
+	echo "replacing: s_${DIRECTORY}_${REPLACEBASELINK}_"
+	fi
+	sed "s_${DIRECTORY}_${REPLACEBASELINK}_" "$TEMP_FILE" > "$OUTPUT_FILE"
+	rm "$TEMP_FILE"
+fi
