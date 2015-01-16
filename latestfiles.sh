732072c16892f5db3f0c0d19db1e1ffae826dd9c
Florian Zeidler <fz@fargerio.de>
2015-01-16 01:03:57 -0500

    icons for many common document types

--- latestfiles.sh
+++ latestfiles.sh
@@ -57,20 +57,20 @@ TEMP_FILE="temp.html"
 START="start.$$"
 END="end.$$"
 
-printf '<html><body><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><link href="' > "$TEMP_FILE"
+printf '<!DOCTYPE HTML>\n<html>\n<head>\n<meta http-equiv="Content-Type" content="text/html; charset=utf-8">\n<link href="' > "$TEMP_FILE"
 echo $THEME >> "$TEMP_FILE"
-printf '.css" media="all" rel="stylesheet" type="text/css" /><script src="sorttable.js"></script></head>\n' >> "$TEMP_FILE"
+printf '.css" media="all" rel="stylesheet" type="text/css" />\n<script src="sorttable.js"></script>\n</head>\n' >> "$TEMP_FILE"
 
 SDATE=$(date +"%Y-%m-%d" --date="1 days ago")
 touch --date "$SDATE" "$START"
-printf '<h1>Today</h1>\n<table class="sortable"><thead><tr><th>type</th><th>name</th><th>size</th><th>time</th></tr></thead>\n' >> "$TEMP_FILE"
+printf '<body>\n<h1>Today</h1>\n<table class="sortable">\n<thead><tr><th>type</th><th>name</th><th>size</th><th>time</th></tr></thead>\n' >> "$TEMP_FILE"
 
 if $VERBOSE; then
 	echo "searching: $DIRECTORY"
 fi
 
 find $DIRECTORY -newer $START -type f \( ! -regex '.*/\..*' \) -printf "<tr><td class=\"%f\"></td><td><a href=\"%p\">%f</a></td><td>%kk</td><td>%TH:%TM</td></tr>\n" >> "$TEMP_FILE"
-printf "</table>/n" >> "$TEMP_FILE"
+printf "</table>\n" >> "$TEMP_FILE"
 I=1
 while [ $I -le $DAYS ]
 do
@@ -81,7 +81,7 @@ do
 	touch --date "$SDATE" "$START"
 	touch --date "$EDATE" "$END"
 
-	printf '<h1>${SDATE}</h1>\n<table class="sortable"><thead><tr><th>type</th><th>name</th><th>size</th><th>time</th></tr></thead>\n' >> "$TEMP_FILE"
+	printf '<h1>%s</h1><table class="sortable">\n<thead>\n<tr><th>type</th><th>name</th><th>size</th><th>time</th></tr></thead>' "$SDATE" >> "$TEMP_FILE"
 
 	if $VERBOSE; then
 		echo "searching: $SDATE - $(( DAYS-I+2 )) day(s) to go"
@@ -89,14 +89,14 @@ do
 
 	find $DIRECTORY -newer $START \! -newer $END -type f \( ! -regex '.*/\..*' \) -printf "<tr><td class=\"%f\"></td><td><a href=\"%p\">%f</a></td><td>%kk</td><td>%Tk:%TM</td></tr>\n" >> "$TEMP_FILE"
 
-	printf "</table>/n" >> "$TEMP_FILE"
+	printf "</table><br /><br />\n" >> "$TEMP_FILE"
 done
 
-echo "</ul></body></html>" >> "$TEMP_FILE"
+printf "</body>\n</html>" >> "$TEMP_FILE"
 /bin/rm -f "$START" 
 /bin/rm -f "$END"
 
-sed -i 's:class\=".*\.\(.*\)">:class=\"\1\">:g' "$TEMP_FILE"
+sed -i 's:class\=".*\.\(.*\)"><\/t:class=\"\1\"><\/t:g' "$TEMP_FILE"
 
 
 if [ "$REPLACEBASELINK" == "" ]; then
