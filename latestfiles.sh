178923e20f4a6f6bcc14d6eb2d31ba455fcbac48
Florian Zeidler <fz@fargerio.de>
2015-01-15 22:38:17 -0500

    table headers & theme selection parameter

--- latestfiles.sh
+++ latestfiles.sh
@@ -1,16 +1,18 @@
 # !/bin/sh 
 # This script will generate a html document listing the newest files in the given filesystem
 # Arguments:
-#	-p [searchpath]    : Path in which to search
-#	-o [output file]   : file to which to write the list
-#   -d [number of days]: number of days in the past to search
-#   -b [new_basepath]  : replace the searchpath in file links with new_basepath
-#   -v 				   : verbose output during execution
+#	-p [searchpath]    	: Path in which to search
+#	-o [output file]   	: file to which to write the list. default: latestfiles.html
+#   -d [number of days]	: number of days in the past to search
+#   -b [new_basepath]  	: replace the searchpath in file links with new_basepath
+#   -v 				   	: verbose output during execution
+#	-t [theme name]		: which theme to use, currently only default exists
 #
 DAYS=7
 REPLACEBASELINK=""
 VERBOSE=false
 OUTPUT_FILE=latestfiles.html
+THEME=default
 while getopts ":p:o:d:b:v" optname
   do
     case "$optname" in
@@ -28,6 +30,9 @@ while getopts ":p:o:d:b:v" optname
 		;;
 	  "v")
 		VERBOSE=true
+		;;
+	  "t")
+		THEME=$OPTARG
 		;;
       "?")
         echo "Unknown option $OPTARG"
@@ -49,22 +54,22 @@ if $VERBOSE; then
 fi
 
 TEMP_FILE="temp.html"
-
 START="start.$$"
 END="end.$$"
 
-echo "<html><body>" > "$TEMP_FILE"
+echo '<html><body><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><link href="$THEME.css" media="all" rel="stylesheet" type="text/css" />
+   </head>' > "$TEMP_FILE"
 
 SDATE=$(date +"%Y-%m-%d" --date="1 days ago")
 touch --date "$SDATE" "$START"
-echo "<h1>Today</h1><ul>" >> "$TEMP_FILE"
+echo "<h1>Today</h1><table><thead><tr><th>type</th><th>name</th><th>size</th><th>time</th></tr></thead>" >> "$TEMP_FILE"
 
 if $VERBOSE; then
 	echo "searching: $DIRECTORY"
 fi
 
-find $DIRECTORY -newer $START -type f \( ! -regex '.*/\..*' \) -printf "<li class=\"%y\"><a href=\"%p\">%f</a> - %kk - %Tk:%TM</li></br>" >> "$TEMP_FILE"
-echo "</ul>" >> "$TEMP_FILE"
+find $DIRECTORY -newer $START -type f \( ! -regex '.*/\..*' \) -printf "<tr><td class=\"%y\"></td><td><a href=\"%p\">%f</a></td><td>%kk</td><td>%Tk:%TM</td></tr>" >> "$TEMP_FILE"
+echo "</table>" >> "$TEMP_FILE"
 I=1
 while [ $I -le $DAYS ]
 do
@@ -75,24 +80,22 @@ do
 	touch --date "$SDATE" "$START"
 	touch --date "$EDATE" "$END"
 
-	echo "<h1>$SDATE</h1><ul>" >> "$TEMP_FILE"
+	echo "<h1>$SDATE</h1><table><thead><tr><th>type</th><th>name</th><th>size</th><th>time</th></tr></thead>" >> "$TEMP_FILE"
 
 	if $VERBOSE; then
 		echo "searching: $SDATE - $(( DAYS-I+2 )) day(s) to go"
 	fi
 
-	find $DIRECTORY -newer $START \! -newer $END -type f \( ! -regex '.*/\..*' \) -printf "<li class=\"%y\"><a href=\"%p\">%f</a> - %kk - %Tk:%TM</li></br>" >> "$TEMP_FILE"
+	find $DIRECTORY -newer $START \! -newer $END -type f \( ! -regex '.*/\..*' \) -printf "<tr><td class=\"%y\"></td><td><a href=\"%p\">%f</a></td><td>%kk</td><td>%Tk:%TM</td></tr>" >> "$TEMP_FILE"
 
-	echo "</ul>" >> "$TEMP_FILE"
+	echo "</table>" >> "$TEMP_FILE"
 done
 
-
 echo "</ul></body></html>" >> "$TEMP_FILE"
-
 /bin/rm -f "$START" 
 /bin/rm -f "$END"
 
-if [ $REPLACEBASELINK == "" ]; then
+if [ "$REPLACEBASELINK" == "" ]; then
 	mv -f "$TEMP_FILE" "$OUTPUT_FILE"
 	echo "not replacing"
 else
