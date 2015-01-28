669fb0435855414731a8fd296aab76c7d0ff6125
Florian Zeidler <fz@fargerio.de>
2015-01-28 18:04:26 -0500

    added mozilla config file as a workaround for local file:/// links not opening

---
+++ user.js
@@ -0,0 +1,4 @@
+user_pref("capability.policy.policynames", "localfilelinks");
+user_pref("capability.policy.localfilelinks.sites", "http://192.168.0.1 http://192.168.1.1");
+user_pref("capability.policy.localfilelinks.checkloaduri.enabled", "allAccess");
+
